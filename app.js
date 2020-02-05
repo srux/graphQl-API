7389c4c11109069ca02ce3e0e328151648d83aa2
srux <samrob.nz@gmail.com>
2020-02-05 14:06:25 +1300

    backend api foundation complete

--- app.js
+++ app.js
@@ -1,131 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql'); // middleware function for express
-const {buildSchema} = require('graphql');
+const graphqlHttp = require('express-graphql');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs')
+const isAuth = require('./middleware/gatekeeper');
 
-const Event = require('./models/event');
-const User = require('./models/user');
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello!');
-})
+app.use(isAuth);
 
-// rootQuery READ rootMutation = CREATE, DELETE, UPDATE graphql lists e.g
-// [String], [Integer], [Boolean]
-
-app.use('/graphql', graphqlHttp({
-    schema: buildSchema(`
-
-            type Event {
-                _id: ID!
-                title: String!
-                description: String!
-                price: Float!
-                date: String!
-            }
-
-            type User {
-                _id: ID!
-                email: String!
-                password: String            
-            }
-
-            input UserInput {
-                email: String!
-                password: String!
-            }
-
-            input EventInput {
-                title: String!
-                description: String!
-                price: Float!
-                date: String!
-            }
-
-            type RootQuery { 
-                events: [Event!]!
-            }
-
-            type RootMutation {
-                createUser(userInput:UserInput):User
-                createEvent(eventInput:EventInput):Event
-            }
-
-            schema {
-                query: RootQuery
-                mutation: RootMutation
-            }
-
-        `),
-    rootValue: {
-        events:() => {
-            return Event.find()
-                .catch(err => {
-                    throw err;
-                })
-            },
-
-        createEvent:(args) => {
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date(args.eventInput.date),
-                creator:'5e38e13c35b3390b88335c83'
-            });
-            return event
-                .save()
-                .then((result) => {
-                    createdEvent = {...result._doc,_id: result._doc._id.toString()}
-                    return User.findById('5e38e13c35b3390b88335c83');
-                })
-                .then(user => {
-                    if (!user) {
-                        throw new Error('User not found.');
-                    }
-                    user.createdEvents.push(event);
-                    return user.save();
-                })
-                .then( result => {
-                    return createdEvent;
-                })
-                .catch((err) => {
-                    console.log(err);
-                    throw err;
-                })
-            },
-
-        createUser: (args) => {
-            return User.findOne({email:args.userInput.email})
-                .then(user => {
-                    if (user) {
-                        throw new Error('User exists already');
-                    }
-                    return bcrypt.hash(args.userInput.password, 12);
-                })
-                .then(hashedPassword => {
-                    const user = new User(
-                        {email: args.userInput.email, password: hashedPassword}
-                    );
-                    return user.save();
-                })
-                .then(result => {
-                    return {...result._doc, password:null, _id: result.id};
-                })
-                .catch(err => {
-                    throw err;
-                });
-            
-        }
-    },
-    graphiql: true // graphql interface for testing e.g http://localhost:4000/graphql
-
-}));
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true
+  })
+);
 
 mongoose
     .connect(
--- models/event.js
+++ models/event.js
@@ -1,29 +1,29 @@
 const mongoose = require('mongoose');
-
+const autopopulate = require('mongoose-autopopulate');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-    title: {
-        type:String,
-        required:true
-    },
-    description: {
-        type:String,
-        required:true
-    },
-    price: {
-        type:Number,
-        required:true
-    },
-    date: {
-        type:Date,
-        default: Date.now,
-        required: true
-    },
-    creator: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  creator: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      autopopulate: true
+  }
 });
-
-module.exports = mongoose.model('Event',eventSchema);
+eventSchema.plugin(autopopulate);
+module.exports = mongoose.model('Event', eventSchema);
